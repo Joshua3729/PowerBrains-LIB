e55b4a901763e27d1a5e2adf39b7377f5f7fad30
Joshua <3729344@myuwc.ac.za>
2022-01-03 22:06:38 +0200

    working on the books component

--- src/Components/tabPages/Books/SingleBook/SingleBook.js
+++ src/Components/tabPages/Books/SingleBook/SingleBook.js
@@ -7,7 +7,10 @@ const singleBook = (props) => {
     <div className={classes.Book}>
       <img src={props.imgUrl} />
       <div className={classes.bookInfo}>
-        <h1>{props.title}</h1>
+        <div className={classes.titleWrapper}>
+          <h1>{props.title.slice(0, 10) + "..."}</h1>
+          <div className={classes.title_preview}>{props.title}</div>
+        </div>
         <p>{props.author}</p>
         <StarRating rating={props.rating} />
         <div className={classes.buttons_wrapper}>
