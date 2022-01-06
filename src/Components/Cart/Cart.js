e8f95c75da98280147d1790e981ea07b4313e489
Joshua <3729344@myuwc.ac.za>
2022-01-06 23:56:57 +0200

    working on the cart functionality

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import classes from "./Cart.module.css";
 import StarRating from "../StarRating/StarRating";
 import emptyCart from "../../Assets/emptyCart.webp";
+import { BrowserRouter } from "react-router-dom";
 
 const cart = (props) => {
   let cartBooks = (
@@ -11,35 +12,45 @@ const cart = (props) => {
       <p>Your Cart Is Empty!</p>
     </div>
   );
+  let borrowBook = null;
 
   if (props.cartData.length > 0) {
-    cartBooks = props.cartData.map((book) => {
-      return (
-        <div key={book._id} className={classes.book}>
-          <div className={classes.leftItems_wrapper}>
-            <img src={book.imgUrl} alt="" />
-            <div className={classes.bookInfo}>
-              <p>{book.title.slice(0, 15) + "..."}</p>
-              <div className={classes.stars}>
-                <StarRating rating={book.rating} />
+    cartBooks = (
+      <div className={classes.CartBooks}>
+        {props.cartData.map((book) => {
+          return (
+            <div key={book._id} className={classes.book}>
+              <div className={classes.leftItems_wrapper}>
+                <img src={book.imgUrl} alt="" />
+                <div className={classes.bookInfo}>
+                  <p>{book.title.slice(0, 15) + "..."}</p>
+                  <div className={classes.stars}>
+                    <StarRating rating={book.rating} />
+                  </div>
+                  <p>{book.author}</p>
+                </div>
               </div>
-              <p>{book.author}</p>
+              <div className={classes.removeItembtn_wrapper}>
+                <button
+                  className={classes.removeItem_btn}
+                  onClick={props.deleteCartItem.bind(this, book)}
+                >
+                  <img
+                    src="https://o.remove.bg/downloads/050ca0f0-88fb-463b-ab29-253c8495372a/5571428-middle-removebg-preview.png"
+                    alt=""
+                  />
+                </button>
+              </div>
             </div>
-          </div>
-          <div className={classes.removeItembtn_wrapper}>
-            <button
-              className={classes.removeItem_btn}
-              onClick={props.deleteCartItem.bind(this, book)}
-            >
-              <img
-                src="https://o.remove.bg/downloads/050ca0f0-88fb-463b-ab29-253c8495372a/5571428-middle-removebg-preview.png"
-                alt=""
-              />
-            </button>
-          </div>
-        </div>
-      );
-    });
+          );
+        })}
+      </div>
+    );
+    borrowBook = (
+      <div className={classes.borrowBooks}>
+        <button className={classes.borrowBook_btn}>BORROW BOOK(S)</button>
+      </div>
+    );
   }
   return ReactDOM.createPortal(
     <div
@@ -50,6 +61,7 @@ const cart = (props) => {
     >
       <h4 className={classes.heading}>Book Cart</h4>
       {cartBooks}
+      {borrowBook}
     </div>,
 
     document.getElementById("side-tray")
