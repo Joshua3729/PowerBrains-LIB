da8ee9f08c51ddc2f33148abbaf1f6a091e2d513
Joshua <3729344@myuwc.ac.za>
2022-01-17 21:24:14 +0200

    implementing the getLoans functionality

--- src/Components/tabPages/Loans/Loans.js
+++ src/Components/tabPages/Loans/Loans.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import classes from "./Loans.module.css";
 
 class Loans extends Component {
+  componentDidMount() {
+    this.props.getLoan();
+  }
   render() {
     return (
       <div className={classes.Loans_wrapper}>
         <h1>Loans</h1>
+        <div className={classes.LoansGrid_wrapper}>
+          <div className={classes.Loan_item}></div>
+          <div className={classes.Loan_item}></div>
+          <div className={classes.Loan_item}></div>
+          <div className={classes.Loan_item}></div>
+        </div>
       </div>
     );
   }
