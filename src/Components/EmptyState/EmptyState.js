4ff65bc373a5f88f4875234428dee1726eba0eae
Joshua <3729344@myuwc.ac.za>
2022-01-26 13:58:45 +0200

    added styling to the emptyState component

--- src/Components/EmptyState/EmptyState.js
+++ src/Components/EmptyState/EmptyState.js
@@ -8,7 +8,7 @@ const emptyState = (props) => {
         src="https://cdni.iconscout.com/illustration/premium/thumb/empty-state-2130362-1800926.png"
         alt=""
       />
-      <p>You Dont Have A Favorite Book. Add Your First Favorite Book Now.</p>
+      <p>You Dont Have Any Favorite Book. Add Your First Favorite Book Now.</p>
     </div>
   );
 };
