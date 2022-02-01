4bd2269d03c9c1733b67c2b643f5ca4fa1cd9b6a
Joshua <3729344@myuwc.ac.za>
2022-02-01 13:48:14 +0200

    working on the count down component

--- src/Components/CountDown/CountDown.js
+++ src/Components/CountDown/CountDown.js
@@ -9,10 +9,10 @@ const calculateTimeLeft = (returnDate) => {
 
   if (difference > 0) {
     timeLeft = {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60),
+      d: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hr: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      min: Math.floor((difference / 1000 / 60) % 60),
+      sec: Math.floor((difference / 1000) % 60),
     };
   }
   return timeLeft;
