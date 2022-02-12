04012fb58648a28696f204d30f60a31ac05d5548
Joshua <3729344@myuwc.ac.za>
2022-02-12 13:30:55 +0200

    working on the view book component

--- src/Components/ViewBook/ViewBook.js
+++ src/Components/ViewBook/ViewBook.js
@@ -1,51 +1,105 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./ViewBook.module.css";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
 import StarRating from "../StarRating/StarRating";
 
-const viewBook = (props) => {
-  return ReactDOM.createPortal(
-    <div
-      className={classes.cartTray}
-      style={{
-        transform: props.openTray ? "translateX(0)" : "translateX(102%)",
-      }}
-    >
-      <button className={classes.exit_btn} onClick={props.clicked}>
-        &times;
-      </button>
-      <div className={classes.bookDetails}>
-        <h4 className={classes.heading}>Book Details</h4>
-        <div className={classes.bookCover}>
-          <img src={props.bookData?.imageUrl} alt="" />
+class ViewBook extends Component {
+  render() {
+    let summary = null;
+    let button = null;
+    if (this.props.openSummary) {
+      summary = (
+        <div
+          className={classes.bookSummaryWrapper}
+          style={{
+            transform: this.props.openSummary
+              ? "translateY(0)"
+              : "translateY(100%)",
+          }}
+        >
+          <button
+            className={classes.closeSummary}
+            onClick={this.props.openSummaryHandler}
+          >
+            &times;
+          </button>
+          <p>{this.props.bookData?.summary}</p>
         </div>
-        <div className={classes.bookInfo}>
-          <p className={classes.bookTittle}>{props.bookData?.name}</p>
-          <p className={classes.authorName}>{props.bookData?.AuthorName}</p>
-          <StarRating rating={props.bookData?.rating} />
-        </div>
-        <div className={classes.bookStatGrid}>
-          <div className={classes.bookStat}>
-            <p className={classes.stat}>{props.bookData?.pages}</p>
-            <p className={classes.statLabel}>Pages</p>
+      );
+    } else {
+      button = (
+        <button
+          className={classes.showSummary}
+          onClick={this.props.openSummaryHandler}
+        >
+          SHOW BOOK SUMMARY
+        </button>
+      );
+    }
+    return ReactDOM.createPortal(
+      <div
+        className={classes.cartTray}
+        style={{
+          transform: this.props.openTray ? "translateX(0)" : "translateX(102%)",
+        }}
+      >
+        <button className={classes.exit_btn} onClick={this.props.clicked}>
+          &times;
+        </button>
+        <div className={classes.bookDetails}>
+          <h4 className={classes.heading}>Book Details</h4>
+          <div className={classes.bookCover}>
+            <img src={this.props.bookData?.imageUrl} alt="" />
           </div>
-          <div className={classes.bookStat}>
-            <p className={classes.stat}>{props.bookData?.release}</p>
-            <p className={classes.statLabel}>Release</p>
+          <div className={classes.bookInfo}>
+            <p className={classes.bookTittle}>{this.props.bookData?.name}</p>
+            <p className={classes.authorName}>
+              {this.props.bookData?.AuthorName}
+            </p>
+            <StarRating rating={this.props.bookData?.rating} />
           </div>
-          <div className={classes.bookStat}>
-            <p className={classes.stat}>{props.bookData?.numberInStock}</p>
-            <p className={classes.statLabel}>Available</p>
+          <div className={classes.bookStatGrid}>
+            <div className={classes.bookStat}>
+              <p className={classes.stat}>{this.props.bookData?.pages}</p>
+              <p className={classes.statLabel}>Pages</p>
+            </div>
+            <div className={classes.bookStat}>
+              <p className={classes.stat}>{this.props.bookData?.release}</p>
+              <p className={classes.statLabel}>Release</p>
+            </div>
+            <div className={classes.bookStat}>
+              <p className={classes.stat}>
+                {this.props.bookData?.numberInStock}
+              </p>
+              <p className={classes.statLabel}>Available</p>
+            </div>
+          </div>
+          <div className={classes.bookSummary}>
+            <p>{this.props.bookData?.summary}</p>
           </div>
         </div>
-        <div className={classes.bookSummary}>
-          <p>{props.bookData?.summary}</p>
+        {button}
+        <div
+          className={classes.bookSummaryWrapper}
+          style={{
+            transform: this.props.openSummary
+              ? "translateY(0)"
+              : "translateY(100%)",
+          }}
+        >
+          <button
+            className={classes.closeSummary}
+            onClick={this.props.openSummaryHandler}
+          >
+            &times;
+          </button>
+          <p>{this.props.bookData?.summary}</p>
         </div>
-      </div>
-    </div>,
+      </div>,
 
-    document.getElementById("side-tray")
-  );
-};
+      document.getElementById("side-tray")
+    );
+  }
+}
 
-export default viewBook;
+export default ViewBook;
