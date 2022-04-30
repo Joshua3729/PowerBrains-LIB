699c13081ae2f11b0d76fd4731c7dce2dd5ce321
Joshua <3729344@myuwc.ac.za>
2022-04-30 21:19:34 +0200

    added to BarChart component

---
+++ src/Components/BarChart/BarChart.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+const barChart = () => {
+  return <p>Im a barchart</p>;
+};
+
+export default barChart;
--- src/Pages/AdminHome/AdminHome.js
+++ src/Pages/AdminHome/AdminHome.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import classes from "./AdminHome.module.css";
+import BarChart from "../../Components/BarChart/BarChart";
 
 class AdminHome extends Component {
   render() {
-    return <h1>Hi Im admin</h1>;
+    return <BarChart />;
   }
 }
 
