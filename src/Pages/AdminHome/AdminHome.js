3185970057c14fce5efabf34c08fa5eaba3c5a16
Joshua <3729344@myuwc.ac.za>
2022-04-30 19:44:04 +0200

    added to AdminHome page

---
+++ src/Pages/AdminHome/AdminHome.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./AdminHome.module.css";
+
+class AdminHome extends Component {
+  render() {
+    return <h1>Hi Im admin</h1>;
+  }
+}
+
+export default AdminHome;
